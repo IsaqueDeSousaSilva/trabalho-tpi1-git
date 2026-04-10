b16d4f1b0c97f98a4924cf7a70d63b8caa0174f5
IsaqueDeSousaSilva <isaque7777sousa@gmail.com>
2026-04-10 18:23:34 -0300

    Corrigir substituição incorreta e restaurar minha parte inicial

--- Calculadora.java
+++ Calculadora.java
@@ -7,35 +7,18 @@ public class Calculadora {
      * @param b Segundo número
      * @return Resultado da operação
      */
-   public double bem(String operacao, double a, double b) {
-
-    switch (operacao.toLowerCase()) {
-        case "soma":
-            return a + b;
-
-        case "subtracao":
-            return a - b;
-
-        case "multiplicacao":
-            return a * b;
-
-        case "divisao":
-            if (b != 0) {
-                return a / b;
-            } else {
-                throw new IllegalArgumentException("Não pode dividir por zero");
-            }
-
-        default:
-            throw new IllegalArgumentException("Operação inválida: " + operacao);
+    public double calcular(String operacao, int a, int b) {
+        switch (operacao.toLowerCase()) {
+            default:
+                throw new IllegalArgumentException("Operação inválida: " + operacao);
+        }
     }
-}
 
     // Método principal para testar
     public static void main(String[] args) {
         Calculadora calc = new Calculadora();
 
-        calc.bem("soma", 10, 5);
+        calc.calcular( "", 0, 0 );
     }
 }
 
